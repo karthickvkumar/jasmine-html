fe969816cd971e819a8f4eee2e46729022075100
kvelraj_concord <kvelraj@concord.net>
2022-07-10 22:22:53 +0530

    update

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -8,7 +8,7 @@ describe("Calculator", function() {
             expect(addition(7, 19)).toBe(26);
         });
 
-        xit("should return an error if we don't supply two numbers", function() {
+        it("should return an error if we don't supply two numbers", function() {
             spyOn(window, "alert");
             addition("Hitchhikers", "Guide")
             expect(window.alert).toHaveBeenCalledWith("Error!");
