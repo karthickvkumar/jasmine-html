65e6d407ac3a29e78d775c1e813b6dcf88e0663b
kvelraj_concord <kvelraj@concord.net>
2022-07-10 20:06:05 +0530

    first commit

--- scripts/calc.js
+++ scripts/calc.js
@@ -10,22 +10,19 @@ Calculator.prototype.add = function(number) {
     }
 };
 
-/*Calculator = function() {
-    this.value = 0;
-};
-
-Calculator.prototype.add = function(number) {
-    if (typeof(number) == "number") {
-        this.value += number;
-    } else {
-        alert("Argument must be a number");
-    }
-};*/
-
-/*function addition(numOne, numTwo) {
+Calculator.prototype.addition = function(numOne, numTwo) {
     if (typeof(numOne) == "number" && typeof(numTwo) == "number") {
         return numOne + numTwo;
     } else {
         return "Error!";
     }
-}*/
+};
+
+
+function addition(numOne, numTwo) {
+    if (typeof(numOne) == "number" && typeof(numTwo) == "number") {
+        return numOne + numTwo;
+    } else {
+        return "Error!";
+    }
+}
